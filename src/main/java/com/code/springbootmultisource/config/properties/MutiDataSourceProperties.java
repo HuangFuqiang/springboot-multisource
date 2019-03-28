abf498cc073d182eb442f55db6e75e62c2c68e71
HuangFuqiang <fuqianghuang01@gmail.com>
2019-03-28 20:41:32 +0800

    [FIX]fix

--- src/main/java/com/code/springbootmultisource/config/properties/MutiDataSourceProperties.java
+++ src/main/java/com/code/springbootmultisource/config/properties/MutiDataSourceProperties.java
@@ -4,12 +4,6 @@ import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-/**
- * 多数据源的配置
- *
- * @author xiongneng
- * @since 2017/6/23 23:05
- */
 @Component
 @ConfigurationProperties(prefix = "biz.datasource")
 public class MutiDataSourceProperties {
